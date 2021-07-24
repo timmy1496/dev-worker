af4bcb98919d87a424a958aeb61fa2723489f7e9
chernenko <artem.chernenko1496@gmail.com>
2021-07-24 23:16:45 +0300

    finish lesson 17

---
+++ api/src/Auth/Command/ChangePassword/Command.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\ChangePassword;
+
+
+class Command
+{
+    public string $id = '';
+    public string $current = '';
+    public string $new = '';
+}
